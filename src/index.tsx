500a9c43316dc2bd695d00f6e58b5aaafa7af0a4
Aditya Tandon <adityatandon007@gmail.com>
2024-01-26 23:02:04 +0530

    cleanups and warning fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+    </Provider>
+  </React.StrictMode>
 );
 
